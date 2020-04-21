3b17c8c7854a35076695d8188533ce300db852e1
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-04-21 22:23:00 +0100

    initial upload

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,4 @@
 library(testthat)
-library(mlr3learnertemplate)
+library(mlr3learners.np)
 
-test_check("mlr3learnertemplate")
+test_check("mlr3learners.np")
--- tests/testthat/test_dens_mixed.R
+++ tests/testthat/test_dens_mixed.R
@@ -1,7 +1,7 @@
-context("<type>.<algorithm>")
+context("dens.mixed")
 
 test_that("autotest", {
-  learner = Learner<algorithm>$new()
+  learner = LearnerDensMixed$new()
   expect_learner(learner)
   result = run_autotest(learner)
   expect_true(result, info = result$error)
