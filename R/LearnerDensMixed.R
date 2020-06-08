9f94cdf8e230b3081dfe79eb5e6231ba2ee5d134
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2020-06-08 10:33:49 +0100

    Update LearnerDensMixed.R

--- R/LearnerDensMixed.R
+++ R/LearnerDensMixed.R
@@ -99,7 +99,7 @@ LearnerDensMixed = R6Class("LearnerDensMixed",
     },
 
     .predict = function(task) {
-      PredictionDens$new(task = task, pdf = self$model$pdf(task$truth()))
+      mlr3proba::PredictionDens$new(task = task, pdf = self$model$pdf(task$truth()))
     }
   )
 )
